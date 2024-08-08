efdbe5a60fb2cf902fc0663ee3ff316d26e1cd18
Ashrib <asharib920@gmail.com>
2024-08-08 20:16:38 +0500

    variables

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,61 @@
-alert("Welcome !");
-alert("Instructions");
+// alert("Welcome !");
+// alert("Instructions");
 
 
-window.alert("Instructions");
+// window.alert("Instructions");
+
+
+var myLastName = "Mark";
+console.log(myLastName);
+
+myLastName = "Ali";
+console.log(myLastName);
+
+
+var names;
+names = "Ace";
+
+var name2 = names;
+
+// alert(names);
+
+
+var name2 = myLastName;
 
 
 
+var x = "text" , y = "",z;
 
+var num = 5 + 5;
+
+var num2 = num + 5;
+console.log(num2);
+
+
+var userMarks  = 85;
+var total  = 100;
+
+var per = userMarks / total * 100;
+
+var num3 = 5;
+var num4 = 10;
+var num5 = num3  + num4;
+
+// alert(2+2);
+
+document.write(num4)
+
+
+var num6 = "5";
+var num7 = num4 - num6;
+
+console.log(num7)
+
+var num$ , num_ ;
+var alert_;
+
+
+
+// alert("hello" + "world")
+
+var a1 = "text"
