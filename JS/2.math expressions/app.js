e73ed43b559cb7e5bd101aa5bf1ef23ee04e976f
Arham <arhamali63@gmail.com>
2024-08-10 19:19:49 +0500

    math expression

---
+++ JS/2.math expressions/app.js
@@ -0,0 +1,34 @@
+var popularNumber = 2 + 2;
+var num = 10; 
+
+var total = num + popularNumber;
+
+// alert(5%3)
+ var num2 = 3;
+ console.log(num2++)
+ console.log(num2)
+ 
+//  num2++;
+//  num2++;
+//  console.log(num2)
+ 
+//  num2 = num2 + 1
+ 
+ num2--;
+ --num2;
+//  console.log(num2)
+
+//  num2 = num2 - 1
+
+var num3 = 5;
+// ++num3
+console.log(++num3)
+
+
+
+var totalCost = ((1 + 3) / 7) * (4%3) + (6 -8);
+
+console.log(totalCost)
+
+
+
