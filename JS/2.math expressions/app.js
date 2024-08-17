f50cdb34785c7ca2eeccc3e57912eaceb53f3264
Ashrib <asharib920@gmail.com>
2024-08-17 20:07:51 +0500

    if statement

--- JS/2.math expressions/app.js
+++ JS/2.math expressions/app.js
@@ -1,34 +1,68 @@
 var popularNumber = 2 + 2;
-var num = 10; 
+var num = 10;
 
 var total = num + popularNumber;
 
 // alert(5%3)
- var num2 = 3;
- console.log(num2++)
- console.log(num2)
- 
+var num2 = 3;
+console.log(num2++);
+console.log(num2);
+
 //  num2++;
 //  num2++;
 //  console.log(num2)
- 
+
 //  num2 = num2 + 1
- 
- num2--;
- --num2;
+
+num2--;
+--num2;
 //  console.log(num2)
 
 //  num2 = num2 - 1
 
 var num3 = 5;
 // ++num3
-console.log(++num3)
+console.log(++num3);
+
+var totalCost = ((1 + 3) / 7) * (4 % 3) + (6 - 8);
+
+// console.log(totalCost)
+
+// var num = 2;
+// var num1 = 1;
+
+// var str = "1";
+// var str1 = "xyz";
+
+// console.log(num + str);
+
+var num1 = 2;
+var num2 = 2;
+
+
+console.log("hello")
 
 
 
-var totalCost = ((1 + 3) / 7) * (4%3) + (6 -8);
+// var pass = prompt("Enter your age");
 
-console.log(totalCost)
+if("18" === 18) // not equal
+{
+    
+    alert("Welcome");
+}
+
+if("18" == 18) // equal
+{
+    
+    alert("Welcome");
+}
 
 
 
+
+
+// console.log(typeof(pass))
+
+
+// console.log("any other code")
