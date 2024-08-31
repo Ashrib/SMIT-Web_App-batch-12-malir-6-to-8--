26a89f16483cbeded01a72f22c9bdb20cfcde671
Ashrib <asharib920@gmail.com>
2024-08-31 19:52:59 +0500

    for loop

--- JS/5. for loops/app.js
+++ JS/5. for loops/app.js
@@ -1,26 +1,54 @@
 
-
 for(var i = 0; i <= 4; i++){
     console.log("hello", i);
-    
 }
 
-
-var citiesArray = [ "karachi", "islamabad", "lahore"];
+var citiesArray = [ "karachi", "islamabad", "islamabad", "lahore"];
 var checkCity = "islamabad";
-
+var isMatched = false;
+var count = 0;
 
 for(var i = 0; i < citiesArray.length; i++){
-    console.log(i);
+    console.log(citiesArray[i]);
     if(checkCity === citiesArray[i]){
-       alert("yes, index: "+ i);
-       break; 
+        alert("yes, index:" + i);
+        isMatched = true;
+        break;
     }
+    //  console.log(citiesArray[i]);
+}
+if(isMatched === false){
+    alert("no");
+}
+
+for(var i = 0; i < citiesArray.length; i++){
+    if(checkCity === citiesArray[i] ){
+        count++;
+    }
+    if(count ){
+        alert("yes"+count);
+        isMatched = true;
+        break;
+    }
+}
+if(isMatched === false){
+    alert("no");
+}
+
+
+for(var i = 1; i <= 10; i+=2){
+    // document.write(i*2+"<br>");
+        console.log("hi" , i)
+    
+} 
+
+for(var i = 10; i >= 1; i--){
+    console.log("hi" , i)
 }
 
 
 
-for(var i = 1; i <= 10; i++){
-    document.write(i*2+"<br>");
-    
-} 
+
+
+
+
