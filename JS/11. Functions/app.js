fb029b3288f241583965edac227d1ab87a574966
Ashrib <asharib920@gmail.com>
2024-09-24 20:07:55 +0500

    function

--- JS/11. Functions/app.js
+++ JS/11. Functions/app.js
@@ -1,64 +1,59 @@
+function greetingUser(passName){
+    console.log(passName)
+    // console.log("Welcome" )
+    // console.log("thanks for visting!")
 
-function tellTime(){
-    var currentTime = new Date();
-    // document.write(currentTime.toString());
-    console.log(currentTime.toString());
-}
-
-tellTime();
-
-
-function consoleUser(){
-    console.log("allow")
-    console.log("welcome")
-    console.log("courses show")
-    console.log("courses show")
-    console.log("courses show")
-    console.log("courses show")
 }
 
 
-function documentUser (){
-        document.write("allow")
-        document.write("welcome")
-        document.write("courses show")
-        document.write("courses show")
-        document.write("courses show")
-        document.write("courses show")
+function totalSum(a){
+    console.log(a)
+    // console.log(a + b)
 }
 
+console.log("before function")
 
-var age = 18
-if(age >= 18){
-    // to console
-    consoleUser();
+// greetingUser();
 
-    // to print screen
-    documentUser() 
-}
 
-if(age < 18){
-    consoleUser();
-}
+// var isLogin = true;
+// if(isLogin === true){
+//     // to greet user
+//     greetingUser();
+// }
 
-function greetingUser(personName) {
+// var loginAfterLong = true
+// if(loginAfterLong === true){
+//      // to greet user
+//      greetingUser();
+//     // ask about plans
+//     askAboutPlans();
+// }
 
-    alert("Welcome "+ personName);
-    alert("Thanks for visting");
-}
 
-var login = true;
-var userName = prompt("enter your name");
-
-console.log(Number(userName))
+var userName = prompt("your name");
+greetingUser(userName)
 
 
 
-if(login === true && userName === "ali"){
-    greetingUser(userName);
-}
-if(login === true && userName === 'asharib'){
-    greetingUser(userName);
-}
 
 
+
+// if(userName === "ali"){
+//     greetingUser(userName);
+// }
+
+// if(userName === "usman"){
+//     greetingUser(userName)
+// }
+
+// totalSum(5,6)
+// totalSum(15,60)
+// totalSum(15,100)
+
+
+
+totalSum('ali')
+totalSum(100)
+totalSum(true)
+totalSum([3,4,6])
--- JS/6. changing case/app.js
+++ JS/6. changing case/app.js
@@ -1,88 +1,95 @@
 var string = "Karachi";
 string = string.toLowerCase();
-// console.log(string)
+
+console.log(string)
 string = string.toUpperCase();
-// console.log(string)
+console.log(string)
 
-// var citiesArray = ["Karachi", "lahore", "islamabad"];
-// var checkCity = prompt("enter city").toLowerCase();
+// // var citiesArray = ["Karachi", "lahore", "islamabad"];
+// // var checkCity = prompt("enter city").toLowerCase();
 
-// for(var i=0; i < citiesArray.length; i++){
-//     if(checkCity === citiesArray[i].toLowerCase()){
-//         alert("yes");
-//     }
-// }
+// // for(var i=0; i < citiesArray.length; i++){
+// //     if(checkCity === citiesArray[i].toLowerCase()){
+// //         alert("yes");
+// //     }
+// // }
 
-// document.write(checkCity);
+// // document.write(checkCity);
 
 // var userName = prompt("enter name").toLowerCase();
-// var firstCh = userName.slice(0,1).toUpperCase();
-// var remainCh = userName.slice(1);
-// var nameToShow = firstCh + remainCh
 
+// document.write(userName)
+
+// var firstCh = userName.slice(0,1).toUpperCase();
 // console.log(firstCh)
-// console.log(remainCh)
+
+// var remainCh = userName.slice(1);
+// var nameToShow = firstCh + remainCh;
+
+// // console.log(firstCh)
+// // console.log(remainCh)
 // document.write(nameToShow)
 
-// var month = prompt("enter month").toLowerCase();
-// var monthToShow = month.slice(0,1).toUpperCase() + month.slice(1,3);
-// document.write(monthToShow);
+// // var month = prompt("enter month").toLowerCase();
+// // var monthToShow = month.slice(0,1).toUpperCase() + month.slice(1,3);
+// // document.write(monthToShow);
 
-// var text = "some  text";
+var text = "some      text";
 
-// for(var i=0; i<text.length; i++){
+for(var i=0; i<text.length; i++){
 
-//     if(text.slice(i, i+2) === "  "){
-//         console.log("double space not allow!")
-//     }
+console.log("index:",i, text.slice(i, i+2));
 
-// }
-
-var text2 = "the New Yorker magazine doesn't allow the phrase World War II. text The New Yorker magazine doesn't allow the phrase World War II. text";
-// for (var i = 0; i < text2.length; i++) {
-//   if (text2.slice(i, i + 12) === "World War II") {
-//     text2 = text2.slice(0, i) + "Second world war" + text2.slice(i + 12);
-//   }
-// }
-
-var findIndex = text2.indexOf("allow");
-
-if (findIndex !== -1) {
-  text2 = text2.slice(0, findIndex) + "not allow" + text2.slice(findIndex + 5);
+    if(text.slice(i, i+2) === "  "){
+        console.log("double space not allow!")
+    }
 }
-console.log(text2);
 
+// var text2 = "the New Yorker magazine doesn't allow the phrase World War II. text The New Yorker magazine doesn't allow the phrase World War II. text";
+// // for (var i = 0; i < text2.length; i++) {
+// //   if (text2.slice(i, i + 12) === "World War II") {
+// //     text2 = text2.slice(0, i) + "Second world war" + text2.slice(i + 12);
+// //   }
+// // }
 
-console.log(text2.lastIndexOf("the"));
+// var findIndex = text2.indexOf("allow");
 
-text2 = text2.replace("the", "and");
-console.log(text2);
-
-// to change global
-text2 = text2.replace(/the/g , "and");
-
-
-// var count = 0;
-// for (var i = 0; i < text2.length; i++) {
-//   if(text2.slice(i, i+3) === "and"){
-//     count++;
-//     if(count === 3){
-//       text2 = text2.slice(0,i) + "the" + text2.slice(i+3);
-//     }
-//   }
+// if (findIndex !== -1) {
+//   text2 = text2.slice(0, findIndex) + "not allow" + text2.slice(findIndex + 5);
 // }
+// console.log(text2);
 
 
-// var a = text2.indexOf("and")
-// console.log(a)
-// for (var i = a+3 ; i < text2.length; i++) {
-//   if(text2.slice(i, i+3) === "and"){
-//       text2 = text2.slice(0,i) + "the" + text2.slice(i+3);
-//       break;
-//   }
-// }
+// console.log(text2.lastIndexOf("the"));
 
-console.log(text2.charAt(9));
-console.log(text2[9]);
+// text2 = text2.replace("the", "and");
+// console.log(text2);
+
+// // to change global
+// text2 = text2.replace(/the/g , "and");
+
+
+// // var count = 0;
+// // for (var i = 0; i < text2.length; i++) {
+// //   if(text2.slice(i, i+3) === "and"){
+// //     count++;
+// //     if(count === 3){
+// //       text2 = text2.slice(0,i) + "the" + text2.slice(i+3);
+// //     }
+// //   }
+// // }
+
+
+// // var a = text2.indexOf("and")
+// // console.log(a)
+// // for (var i = a+3 ; i < text2.length; i++) {
+// //   if(text2.slice(i, i+3) === "and"){
+// //       text2 = text2.slice(0,i) + "the" + text2.slice(i+3);
+// //       break;
+// //   }
+// // }
+
+// console.log(text2.charAt(9));
+// console.log(text2[9]);
 
 
