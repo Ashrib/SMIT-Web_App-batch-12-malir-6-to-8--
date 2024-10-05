5acdb24193954bc70d2667b6c2be33cd317dfa52
Ashrib <asharib920@gmail.com>
2024-10-05 20:06:34 +0500

    functions and create and append elements

--- JS/11. Functions/app.js
+++ JS/11. Functions/app.js
@@ -168,3 +168,18 @@ var userInp = prompt("");
   console.log(addToArray(354))
 
 
+
+
+  /// 
+    // 5!
+    // 5*4*3*2*1
+
+
+function factorial(num){
+    if(num <= 1 ){
+        return 1;
+    }
+    return num * factorial(num-1);
+
+}
+console.log(factorial(4));
--- JS/12. DOM/app.js
+++ JS/12. DOM/app.js
@@ -1,18 +1,56 @@
+function toggleTheme(){
+    var bodyElm = document.getElementsByTagName("body")[0];
+    var bodyClass = bodyElm.getAttribute("class");
+    console.log(bodyElm.getAttribute("class"))
+
+    if(bodyClass === "light"){
+        bodyElm.setAttribute("class", "dark")
+    }
+    else{
+        bodyElm.setAttribute("class", "light")
+    }
+}
+
+
+
+
+
 var mainDiv = document.getElementById("main")
 console.log(mainDiv)
 
 // var userName = prompt("enter your name");
 // heading.innerText = userName;
 
-console.log(heading)
+// console.log(heading)
 
 
-var users = [ 'ali', 'hassan', 'arbaz']
-for (let i = 0; i < users.length; i++) {
-    var heading = document.createElement("h1");
-    heading.setAttribute("class", "red")
-    heading.innerText = users[i];
+var users = [ 'ali', 'hassan', 'arbaz'];
+
+// for (let i = 0; i < users.length; i++) {
+//     var heading = document.createElement("h1");
+//         heading.innerText = users[i];
+//         heading.setAttribute("class", "red")
+//         heading.setAttribute("id", "abc")
+
+//         mainDiv.appendChild(heading);
+
     
-    mainDiv.appendChild(heading)
-    
+// }
+
+
+
+
+var tableElm = document.createElement("table");
+tableElm.setAttribute("border", "1")
+for (let i = 0; i < 5; i++) {
+    var tableRow1 = document.createElement("tr");
+    var tbCol = document.createElement("td");
+    var tbCol2 = document.createElement("td");
+    tbCol.innerText = "col 1";
+    tbCol2.innerText = "col 2";
+    tableRow1.appendChild(tbCol)
+    tableRow1.appendChild(tbCol2)
+    tableElm.appendChild(tableRow1)
 }
+
+mainDiv.appendChild(tableElm)
