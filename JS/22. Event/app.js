1b2d3ad90e84cd64c1ee660f9273d574e2c1c70b
muhammadasif46 <asifahmed32c@gmail.com>
2024-11-12 19:55:51 +0500

    add class,  show more text

--- JS/22. Event/app.js
+++ JS/22. Event/app.js
@@ -1,11 +1,10 @@
 function change(e, check) {
   // console.log(check);
   // console.log(e.target.getAttribute("src"));
-  if (check === "over") { 
+  if (check === "over") {
     e.target.setAttribute("src", "./03.PNG");
     return;
   }
-
   e.target.setAttribute("src", "./02.jpg");
 }
 
@@ -13,45 +12,61 @@ function focusInp(e) {
   console.log(e.target);
   e.target.style.background = "lightgreen";
 }
-
-function blurInp(e , checkInp) {
-  
-  console.log(e.target.value)
-  var nameError = document.getElementsByClassName('name-error')[0];
-  var passError = document.getElementsByClassName('pass-error')[0];
+function validateInp(e, checkInp) {
+  console.log(e.target.value);
+  var nameError = document.getElementsByClassName("name-error")[0];
+  var passError = document.getElementsByClassName("pass-error")[0];
+  var emailError = document.getElementsByClassName("email-error")[0];
   // console.log(e.target.parentNode.childNodes );
   // console.log(nameError );
   e.target.style.background = "transparent";
-  if(checkInp === 'name'){
-    
-  if(e.target.value.length < 3){
-    nameError.innerText = 'name should be 3 char'
-    nameError.style.display = 'block';
-    return;
-  }
-  nameError.style.display = 'none';
-  }
-
-  if(checkInp === 'pass'){
-    if(e.target.value.length < 9){
-      passError.innerText = 'password is weak'
-      passError.style.display = 'block';
+  if (checkInp === "name") {
+    if (e.target.value.length < 3) {
+      nameError.innerText = "name should be 3 char";
+      nameError.style.display = "block";
       return;
     }
-    passError.style.display = 'none';
+    nameError.style.display = "none";
+  }
+
+  if (checkInp === "pass") {
+    if (e.target.value.length < 9) {
+      passError.innerText = "password is weak";
+      passError.style.display = "block";
+      return;
+    }
+    passError.style.display = "none";
+  }
+
+  if (checkInp === "email") {
+    if (e.target.value.indexOf("@") === -1) {
+      emailError.innerText = "invalid email";
+      emailError.style.display = "block";
+      return;
+    }
+    emailError.style.display = "none";
   }
 }
 
-
-function passVisiblility(e){  // to password show or hide
+function passVisiblility(e) {  //password show/hide
+  // to password show or hide
   // console.log(e.target.previousSibling)
-  if(e.target.innerText == 'show'){
-    e.target.previousSibling.setAttribute('type', 'text')
-    e.target.innerText = 'hide'; // toggle = show
-    return; 
+  if (e.target.innerText == "show") {
+    e.target.previousSibling.setAttribute("type", "text");
+    e.target.innerText = "hide"; // toggle = show
+    return;
   }
-  e.target.innerText = 'show'; // toggle = show
-  e.target.previousSibling.setAttribute('type', 'password')
-
-
+  e.target.innerText = "show"; // toggle = show
+  e.target.previousSibling.setAttribute("type", "password");
+}
+function addStyle(e) {
+  if (e.target.className.indexOf("redText") === -1) { // to check there is class redText already exist or not; if -1:not exist, if any index return:exist
+    e.target.className += " redText"; // add another class
+    return; // return here
+  }
+  e.target.className = e.target.className.replace("redText", ""); // to remove redText class if already exist
+}
+function showMore() { // show more text
+  document.getElementById("para").innerText =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis voluptatibus aperiam, facere commodi ab ex quibusdam unde doloribus delectus vitae. Inventore expedita eius dolor eum minus assumenda blanditiis sequi doloremque.";
 }
