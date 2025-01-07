026bd67378598f5c190614b4460263f7d979e637
Ashrib <asharib920@gmail.com.com>
2025-01-07 20:10:57 +0500

    data rendering with api and filter data

---
+++ JS/34. render data with api/app.js
@@ -0,0 +1,55 @@
+let allData = [];
+let getData = async () => {
+  try {
+    let data = await fetch(
+      "http://universities.hipolabs.com/search?country=United+States"
+    );
+    data = await data.json();
+    // console.log(data);
+    allData = [...data];
+  } catch (error) {
+    console.error(error);
+  }
+};
+getData().then(()=>{
+  renderData() //
+});
+
+let renderData = ()=>{
+  if(allData){
+    // console.log(allData);
+    allData.length = 20
+    document.querySelector(".main").innerHTML = ''
+    allData?.map((obj) => {
+      document.querySelector(".main").innerHTML += `
+      <div class='box'>
+      ${obj?.name}
+      <div>
+      ${obj?.domains[0]}
+      </div>
+      </div>
+      `;
+    });
+  }
+  }
+renderData();
+
+
+let getFilter = (value)=>{
+  console.log(value)
+  if(!value.length<1) {
+    let getfilterData = allData?.filter(obj => obj?.name.toLowerCase() == value.toLowerCase())
+    if(getfilterData){
+      document.querySelector(".main").innerHTML = `
+      <div class='box'>
+      ${getfilterData[0]?.name || 'no data'}
+      <div>
+      ${getfilterData[0]?.domains[0] || 'no data'}
+      </div>
+      </div>
+      `; 
+      return
+    }
+  }
+  renderData();
+}
